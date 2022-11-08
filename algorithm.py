4f574b09f686c4eef4c5fb0f099900abd6b6473a
Parth Jain <parthljain2002@gmail.com>
2022-11-08 16:49:08 +0530

    Refactored code and changed region mapping

--- algorithm.py
+++ algorithm.py
@@ -164,4 +164,34 @@ class Hierarchy:
                     cur = triangle
                     break
         print(cur)
-        return self.regions.get(cur, "External Region")
+        return self.regions.get(self.poly_map[cur], "External Region")
+
+
+def Algorithm(regions: list[Polygon], region_names: list[str]):
+    H = Hierarchy()
+
+    for region, name in zip(regions, region_names):
+        H.add_region(region, name)
+    
+    H.triangulate()
+
+    while (len(H.triangulation) > 3):
+        independent_set = H.select_independent_set()
+
+        print(independent_set)
+        # print(H.triangulation)
+
+        plot_polys(H.triangulation)
+        for polygon in H.polys:
+            plot_poly(polygon, 'r-')
+        for vertex in independent_set:
+            print(f"removing {vertex}")
+            H.remove_point(vertex)
+
+    plot_polys(H.triangulation)
+    print(len(H.adj.keys()))
+
+    while (True):
+        x = int(input())
+        y = int(input())
+        print(H.search_point(Point(x, y)))
--- test.py
+++ test.py
@@ -1,35 +1,6 @@
 from algorithm import *
 import json
 
-def Algorithm(regions: list[Polygon], region_names: list[str]):
-    H = Hierarchy()
-
-    for region, name in zip(regions, region_names):
-        H.add_region(region, name)
-    
-    H.triangulate()
-
-    while (len(H.triangulation) > 3):
-        independent_set = H.select_independent_set()
-
-        print(independent_set)
-        # print(H.triangulation)
-
-        plot_polys(H.triangulation)
-        for polygon in H.polys:
-            plot_poly(polygon, 'r-')
-        for vertex in independent_set:
-            print(f"removing {vertex}")
-            H.remove_point(vertex)
-
-    plot_polys(H.triangulation)
-    print(len(H.adj.keys()))
-
-    while (True):
-        x = int(input())
-        y = int(input())
-        print(H.search_point(Point(x, y)))
-
 if __name__ == "__main__":
     P1 = Polygon((20, 0),(-5, 41),(-20, 10),(-20, -20))
     P2 = Polygon((-75, 75),(-50, 25),(-75, -75))
