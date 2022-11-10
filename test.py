3bfb1ac89e8ea44e1e808cb6260deecd45f0b886
prathamkundan <2020csb1114@iitrpr.ac.in>
2022-11-10 15:25:34 +0530

    minor changes

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 from algorithm import *
-import json
 
 if __name__ == "__main__":
     P1 = Polygon((20, 0),(-5, 41),(-20, 10),(-20, -20))
     P2 = Polygon((-75, 75),(-50, 25),(-75, -75))
-    regions = [P2]
+    regions = [P1, P2]
     region_names = ["R1","R2"]
     Algorithm(regions, region_names)
